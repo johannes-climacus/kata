7eb9719477584357d8b4e2fa82958edade137776
Johannes Climacus <johannesclimacus@tuta.io>
2019-05-16 15:20:05 -0400

    Use match

--- greed/greed.rkt
+++ greed/greed.rkt
@@ -1,43 +1,26 @@
 #lang racket
 
 (define (score rolls)
-  (define (tally face rolls)
-    (length (filter (lambda (n) (= n face)) rolls)))
-  (define (rule matches face points)
-    (if (= matches (tally face rolls)) points 0))
-  (apply + (list (rule 1 1  100)
-                 (rule 1 5   50)
-                 (rule 2 1  200)
-                 (rule 2 5  100)
-                 (rule 3 1 1000)
-                 (rule 3 2  200)
-                 (rule 3 3  300)
-                 (rule 3 4  400)
-                 (rule 3 5  500)
-                 (rule 3 6  600)
-                 (rule 4 1 1100)
-                 (rule 4 5  550)
-                 (rule 5 1 1200)
-                 (rule 5 5  600))))
-
-(define (play)
-  (define (rolls n [l empty])
-    (define (roll) (+ (random 6) 1))
-    (if (zero? n)
-        l
-        (begin
-          (set! l (cons (roll) l))
-          (rolls (- n 1) l))))
-  (define luck (rolls 5))
-  (displayln (format "Rolled: ~a" luck))
-  (displayln (format "Scored: ~a" (score luck))))
+  (define (rule face single double triple quadruple quintuple)
+    (match (length (filter (lambda (n) (= n face)) rolls))
+      [1 single]
+      [2 double]
+      [3 triple]
+      [4 quadruple]
+      [5 quintuple]
+      [_ 0]))
+  (apply + (list (rule 1 100 200 1000 1100 1200)
+                 (rule 2   0   0  200    0    0)
+                 (rule 3   0   0  300    0    0)
+                 (rule 4   0   0  400    0    0)
+                 (rule 5  50 100  500  550  600)
+                 (rule 6   0   0  600    0    0))))
 
 (displayln
- (if (and
-      (= (score '(1 1 1 5 1)) 1150)
-      (= (score '(2 3 4 6 2))    0)
-      (= (score '(3 4 5 3 3))  350)
-      (= (score '(1 5 1 2 4))  250)
-      (= (score '(5 5 5 5 5))  600))
+ (if (and (= (score '(1 1 1 5 1)) 1150)
+          (= (score '(2 3 4 6 2))    0)
+          (= (score '(3 4 5 3 3))  350)
+          (= (score '(1 5 1 2 4))  250)
+          (= (score '(5 5 5 5 5))  600))
      "Tests passed"
      "Tests failed"))
