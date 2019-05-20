e826112c8799d6e616fea6aca877ff4adf71980e
Johannes Climacus <johannesclimacus@tuta.io>
2019-05-20 18:39:17 -0400

    Add AWK FizzBuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -2,10 +2,10 @@ const range = (i, j) => [...Array(j).keys()].filter(x => x >= i);
 
 const fizzbuzz = i => {
   const dividesp = (x, y) => (x % y) === 0;
-  if (dividesp(i, 15)) { return "FizzBuzz" }
-  else if (dividesp(i, 5)) { return "Buzz"}
-  else if (dividesp(i, 3)) { return "Fizz"}
-  else { return i };
+  if      (dividesp(i, 15)) { return "FizzBuzz" }
+  else if (dividesp(i,  5)) { return "Buzz"}
+  else if (dividesp(i,  3)) { return "Fizz"}
+  else                      { return i };
 };
 
 range(1, 101).forEach(x => console.log(fizzbuzz(x)));
