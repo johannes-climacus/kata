23eabd20e99a751a20f30f621a73596cc23ff4b8
Johannes Climacus <johannesclimacus@tuta.io>
2019-06-02 11:51:30 -0400

    Fixup smallest-multiple answer

--- math-problems/smallest-multiple.rkt
+++ math-problems/smallest-multiple.rkt
@@ -1,17 +1,26 @@
 #lang racket
 
-(define (multiple? i j) (= (remainder i j) 0))
+; old busted answer for reference
+;(define (multiple? i j) (= (remainder i j) 0))
+;
+;(define (multiple?-through check through [results empty])
+;  (for/and ([each (in-naturals)]
+;             #:when  (> each 0)
+;             #:break (= (+ 1 through) each))
+;    (multiple? check each)))
+;
+;(define (goal? n) (multiple?-through n 20))
+;
+;(for ([n (in-naturals)]
+;      #:break (= n 300000000)
+;      #:when  (> n 0)
+;      #:when  (goal? n))
+;  (displayln n))
 
-(define (multiple?-through check through [results empty])
-  (for/and ([each (in-naturals)]
-             #:when  (> each 0)
-             #:break (= (+ 1 through) each))
-    (multiple? check each)))
+; new "cheating" answer
 
-(define (goal? n) (multiple?-through n 20))
+(define (lcm-range through)
+  (apply lcm (for/list ([n (in-range 1 through)]) n)))
 
-(for ([n (in-naturals)]
-      #:break (= n 300000000)
-      #:when  (> n 0)
-      #:when  (goal? n))
-  (displayln n))
+; test
+(= (lcm-range 10) 2520)
