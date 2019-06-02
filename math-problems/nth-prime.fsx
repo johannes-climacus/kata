0f00c661617b8d5ed36066a92841ef914cfd22c4
Johannes Climacus <johannesclimacus@tuta.io>
2019-06-02 12:19:02 -0400

    Add nth-prime F#

---
+++ math-problems/nth-prime.fsx
@@ -0,0 +1,32 @@
+let isDivisible i j = i % j = 0I ;;
+let isEven i = isDivisible i 2I ;;
+let isOdd i = not (isEven i) ;;
+let bigIntSqrt (i : bigint) = i |> float |> sqrt |> bigint ;;
+
+let factors i = Set [1I..i] |> Set.filter(isDivisible i) ;;
+
+let sieveOfEratosthenes (limit : bigint) =
+  let baseRange =
+    [2I..limit]
+    |> Set.ofSeq
+    |> Set.filter(fun i -> i = 2I || isOdd i)
+
+  let limitedSquares =
+    baseRange
+    |> Set.filter(fun i -> i < bigIntSqrt limit)
+    |> Set.toList
+
+  let rec crossOff squares set =
+    let filterer i j = not (isDivisible i j) || i = j
+    match squares with
+    | [] -> set
+    | h :: t -> crossOff t (set |> Set.filter(fun x -> filterer x h))
+
+  crossOff limitedSquares baseRange
+;;
+
+sieveOfEratosthenes 150000I
+|> Set.toSeq
+|> Seq.item 10000
+;;
+
