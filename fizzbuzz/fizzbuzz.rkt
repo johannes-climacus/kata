10bb4b96f477dcd4acc6ac914291bd6666214578
Johannes Climacus <johannesclimacus@tuta.io>
2019-06-02 10:36:34 -0400

    Add guards to racket fizzbuzz

--- fizzbuzz/fizzbuzz.rkt
+++ fizzbuzz/fizzbuzz.rkt
@@ -1,14 +1,17 @@
 #lang racket
+(require threading)
 
-(define (fizzbuzz i)
-  (define (filter n)
+(define (fizzbuzz x)
+  (define (buzzer n)
     (match (gcd n 15)
       [15 "FizzBuzz"]
       [5 "Buzz"]
       [3 "Fizz"]
       [_ n]))
 
-  (for ([n (in-range 1 (+ i 1))]) (displayln (filter n))))
+  (for ([n (in-naturals)]
+        #:when  (> n      0)
+        #:break (= n (+ 1 x)))
+    (~> n buzzer displayln)))
 
 (fizzbuzz 100)
-
