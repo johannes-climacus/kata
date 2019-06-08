a5ab0102b6caf7aeabe2fab624fafccc4951e583
Johannes Climacus <johannesclimacus@tuta.io>
2019-06-08 15:42:08 -0400

    Add champ F# solution

---
+++ math-problems/champernowes-constant.fsx
@@ -0,0 +1,19 @@
+let champer range nths =
+  let digitsOfRange =
+    range
+    |> List.map string
+    |> String.concat ""
+    |> List.ofSeq
+    |> List.map(fun x -> int x - 48)
+  let rec loop range nths acc =
+    match nths with
+    | []     -> acc
+    | h :: t -> loop range t (acc @ [(digitsOfRange |> List.item (h - 1))])
+  loop range nths []
+  |> List.reduce (*)
+
+champer
+  [1..1000000]
+  [1; 10; 100; 1000; 10000; 100000; 1000000]
+|> printfn "%i"
+
